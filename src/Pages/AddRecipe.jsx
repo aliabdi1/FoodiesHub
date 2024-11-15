b5a19776af89ddd8a0133b8d84f73cccda9aa5d9
flakegotgame <victorchange808@gmail.com>
2024-11-15 22:10:35 +0300

    AppRecipe

---
+++ src/Pages/AddRecipe.jsx
@@ -0,0 +1,10 @@
+import RandomRecipe from "../components/RandomRecipe";
+
+const AddRecipe = () => (
+  <div>
+    <h1 className="text-xl font-bold mb-4">Discover a Random Recipe</h1>
+    <RandomRecipe />
+  </div>
+);
+
+export default AddRecipe;
