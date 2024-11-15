8eefcb8eb8dc66b0d13572f711a89cbeefd1e53d
flakegotgame <victorchange808@gmail.com>
2024-11-15 22:08:10 +0300

    Home.jsx

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import RecipeList from "../components/RecipeList";
+
+const Home = () => (
+  <div>
+    <h1 className="text-xl font-bold mb-4">Find Your Favorite Recipes</h1>
+    <RecipeList />
+  </div>
+);
+
+export default Home;
