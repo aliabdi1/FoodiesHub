ee60486c265fc75ec64db0f1eaf024bdb7668759
flakegotgame <victorchange808@gmail.com>
2024-11-16 00:37:18 +0300

    recipecard

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => (
+    <footer className="bg-blue-500 py-4">
+      <div className="container mx-auto text-center text-white">
+        <p>© 2024 FoodiesHub. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+  
+  export default Footer;
+  
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,12 +2,11 @@ const About = () => (
     <div>
       <h1 className="text-xl font-bold mb-4">FoodieHub</h1>
       <p>
-        FoodieHub is an app for food lovers filled with a variety of recepies to keep your culinary experiences filled with excitment.
-        Created by Chef Ali and Victor.
-        
+        <h1></h1>FoodieHub is an app for food lovers filled with a variety of recepies to keep your culinary experiences filled with excitment.
       </p>
     </div>
   );
   
   export default About;
+
   
