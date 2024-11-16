76d4cd86cac7858c997ed09038d580126c7b5fbb
flakegotgame <victorchange808@gmail.com>
2024-11-16 11:49:13 +0300

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,32 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => (
-  <nav className="bg-green-500 p-4 text-white">
-    <Link to="/" className="mr-4">Home</Link>
-    <Link to="/add-recipe" className="mr-4">Add Recipe</Link>
-    <Link to="/about">About</Link>
+  <nav className="bg-green-500 px-6 py-4 flex items-center justify-between shadow-lg">
+    {/* Project Name */}
+    <div className="text-xl font-bold text-white">
+      FoodiesHub
+    </div>
+    {/* Navigation Links */}
+    <div className="flex space-x-6">
+      <Link 
+        to="/" 
+        className="hover:underline text-white font-medium"
+      >
+        Home
+      </Link>
+      <Link 
+        to="/add-recipe" 
+        className="hover:underline text-white font-medium"
+      >
+        Add Recipe
+      </Link>
+      <Link 
+        to="/about" 
+        className="hover:underline text-white font-medium"
+      >
+        About
+      </Link>
+    </div>
   </nav>
 );
 
